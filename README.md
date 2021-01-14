92121114b98529a551adfe9b89041888e3aa35e4
Chelsea Houston <chelseahouston29@gmail.com>
2021-01-14 11:46:45 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Change Text to Several Criteria
+A Ruby program which will shift text to this criteria:
+a) lowercase to uppercase 
+b) capitalise a sentence
+c) swap the case over
+d) reverse the text
+
+## Installation
+No installation required to run this project.
+```
+Ruby main.cpp
+```
+
+Created as part of TechTalent Academy Women in Software course.
---
+++ main.rb
@@ -0,0 +1,30 @@
+
+# Lowercase to Uppercase ---
+puts "Lowercase to Uppercase:"
+string1 = "hello everyone"
+puts "String: '#{string1}'"
+puts string1.upcase
+
+puts "\n" # separation ---
+
+# Capitalize a sentence ---
+puts "Capitalize a Sentence:"
+string1 = "hello everyone"
+puts "String: '#{string1}'"
+puts string1.capitalize
+
+puts "\n" # separation ---
+
+# Swap the case over ---
+puts "Swap the case over:"
+string2 = "hello EVERYONE"
+puts "String: '#{string2}'"
+puts string2.swapcase
+
+puts "\n" # separation ---
+
+# Reverse the text ---
+puts "Reverse the Text"
+string2 = "hello everyone"
+puts "String: '#{string2}'"
+puts string2.reverse
